f6640038e3799ace1041cddbd09f1943c4a1d0c0
TranPhuThien <tranpthien2412@gmail.com>
2021-10-23 11:27:35 +0700

    feature 01: entity class, fluent api, add migration, update database

---
+++ PTUD_eShopVPP.Data/EF/EShopVPPDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PTUD_eShopVPP.Data.EF
+{
+    public class EShopVPPDbContextFactory : IDesignTimeDbContextFactory<EShopVPPDbContext>
+    {
+        public EShopVPPDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("PTUD_eShopVPPDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopVPPDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopVPPDbContext(optionsBuilder.Options);
+        }
+    }
+}
